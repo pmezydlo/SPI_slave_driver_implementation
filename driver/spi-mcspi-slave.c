9a99f6791bbddb5666070c786ceda40b86593c2d
Patryk Mezydlo <mezydlo.p@gmail.com>
2016-05-13 12:45:01 +0000

    use omap4 registers offset for determinate base address

--- driver/spi-mcspi-slave.c
+++ driver/spi-mcspi-slave.c
@@ -9,34 +9,62 @@
 
 #include <linux/device.h>
 
+#include "spi-mcspi-slave.h"
+ 
+/* 
+ * define omap4 register offset in platform data
+ */
+static struct mcspi_slave_platform_config mcspi_slave_pdata = {
+    .regs_offset = OMAP4_MCSPI_REG_OFFSET,
+};
+
 static const struct of_device_id mcspi_slave_of_match[] = {
-	{ .compatible = "ti,omap4-mcspi" },
+	{
+            .compatible = "ti,omap4-mcspi",
+            .data = &mcspi_slave_pdata,
+        },
 	{},
 };
 MODULE_DEVICE_TABLE(of, mcspi_slave_of_match);
 
+
 static int mcspi_slave_probe(struct platform_device *pdev){
 
-    dev_dbg(&pdev->dev, "probe\n");
+    printk(KERN_INFO "mcspi_slave probe\n");
+
     const struct of_device_id *of_id = of_match_device(mcspi_slave_of_match, &pdev->dev);
+    const struct mcspi_slave_platform_config *pdata; 
+    unsigned int regs_offset = 0;
 
     if (of_id) {
-
+        pdata = of_id->data;
     }
+    else
+    {
+        pdata = dev_get_platdata(&pdev->dev);
+    }
+
+    regs_offset = pdata->regs_offset;
         
     struct resource *res1;
 
     res1 = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 
-    dev_dbg(&pdev->dev, "Start:%x,  End:%x Size:%d\n", (unsigned long)res1->start, (unsigned long)res1->end, resource_size(res1));
+    //dev_dbg(&pdev->dev, "Start:%x,  End:%x Size:%d  Offset:%x \n ", (unsigned long)res1->start, (unsigned long)res1->end, resource_size(res1),(unsigned int)regs_);
+    
+    printk(KERN_INFO "Start:%x,  End:%x Size:%d  Offset:%x \n ", (unsigned long)res1->start, 
+                                                                 (unsigned long)res1->end, 
+                                                                 resource_size(res1),
+                                                                 (unsigned int)regs_offset);
 
     return 0;
 }
 
 static int mcspi_slave_remove(struct platform_device *pdev){
-	dev_dbg(&pdev->dev, "driver remove\n");
+//	dev_dbg(&pdev->dev, "driver remove\n");
 	return 0;
 }
+
 
 static struct platform_driver mcspi_slave_driver = {
 	.probe =	mcspi_slave_probe,
@@ -49,11 +77,11 @@ static struct platform_driver mcspi_slave_driver = {
 
 static int __init mcspi_slave_init(void){
 
-	int ret;
-
-	ret = platform_driver_register(&mcspi_slave_driver);
-
-	if (ret != 0) {
+        int ret;
+        printk(KERN_INFO "mcspi_slave init \n");
+        ret = platform_driver_register(&mcspi_slave_driver);
+        
+        if (ret != 0) {
 
 	}
 	else{
@@ -65,7 +93,7 @@ static int __init mcspi_slave_init(void){
 
 static void __exit mcspi_slave_exit(void){
 	platform_driver_unregister(&mcspi_slave_driver);
-
+        printk(KERN_INFO "mcspi_slave exit \n");
 	return;
 }
 
