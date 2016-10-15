4b02fc227e14b4cbe792085444d60d71fd502639
Patryk <mezydlo.p@gmail.com>
2016-10-16 00:58:42 +0200

    fixed travis script issues
    Signed-off-by: Patryk <mezydlo.p@gmail.com>

--- firmware/gen/pru_spi_slave.pp
+++
@@ -1,4 +0,0 @@
-gen/pru_spi_slave.object: pru_spi_slave.c
-gen/pru_spi_slave.object: /usr/share/ti/cgt-pru/include/stdint.h
-gen/pru_spi_slave.object: ./include/resource_table_empty.h
-gen/pru_spi_slave.object: ./include/pru_cfg.h
